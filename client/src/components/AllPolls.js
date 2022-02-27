0a06de4ff34f2103eefcd8af93d04bbe85815808
ianseki <ian.m.seki@gmail.com>
2022-02-26 23:03:17 -0800

    first test commit changed App.js and added component folder

---
+++ client/src/components/AllPolls.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Link, navigate } from '@reach/router';
+
+const AllPolls = (props) =>
+{
+    
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default AllPolls;
